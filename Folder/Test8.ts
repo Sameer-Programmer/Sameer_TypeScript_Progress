f1e90c7be2c4d7ece56746e89350541a59e702ca
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-09 23:15:37 +0530

    C2

---
+++ Folder/Test8.ts
@@ -0,0 +1,10 @@
+var x1 =10;
+var x1 =20;
+
+
+
+// let y1 =20;
+// let y1 = 30;  throw an error
+
+// const z1 = 40;
+// const z1 = 40; throw an erro
---
+++ Folder/Test9.ts
@@ -0,0 +1,8 @@
+// Declaration 
+var x2 = 10;
+let y2 =20;
+const z2 =30;
+
+console.log(x2)
+console.log(y2)
+console.log(z2)
