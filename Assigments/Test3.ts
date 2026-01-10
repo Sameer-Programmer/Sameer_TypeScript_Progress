df93bcda3597da7ea8a3bc973cce77b5b06e2532
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-10 23:29:00 +0530

    Jan-10-2026 11:28:PM

---
+++ Assigments/Test3.ts
@@ -0,0 +1,13 @@
+
+/*Loop
+initialization 
+condtion 
+increent or decrement 
+while loop always checks the condition is true then executes
+*/
+
+let i : number = 0; // initialization 
+while (i<=10){
+    console.log(i)
+    i++;
+}
---
+++ Assigments/Test4.ts
@@ -0,0 +1,14 @@
+/*Loop
+initialization 
+condtion 
+increent or decrement 
+while loop always checks the condition is true then executes
+do while loop insted of checking the Condition executes atlest once
+*/
+
+let i : number = 0; // initialization 
+
+do {
+    console.log(i);
+    i++;
+}while(i<1);
