33d83b74a17d308258a6dd63071a6c60c78469bd
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-10 11:46:41 +0530

    m

---
+++ Folder2/Test4.ts
@@ -0,0 +1,11 @@
+// Terinary Operator
+
+// Expression  ?:
+let a =10;
+let b =20;
+
+let result = a>b ? a : b ;
+console.log(result)
+
+// if expression or condiiton  is true a will be assigned to Result 
+// if expression or condiiton  is false  b will be assigned to Result 
