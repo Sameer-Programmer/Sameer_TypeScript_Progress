60f8f6ad8364bdfc48331f38594682addad4b9d8
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-10 16:25:39 +0530

    "Jan-10-2026 04:25:PM"

---
+++ Assigments/Test1.ts
@@ -0,0 +1,9 @@
+// check if a number is multiples of 10 
+
+ let value : number = 150
+ 
+ if(value % 10 ==0 ){
+     console.log(`${value} is multile of 10 `)
+ }else {
+     console.log(`${value} is Not  multile of 10 `)
+ }
---
+++ Assigments/Test2.ts
@@ -0,0 +1,8 @@
+let num : number = -0;
+if (num >0 ){
+    console.log(`${num} is a positive number`)
+}else if (num <0) {
+      console.log(`${num} is a negative  number`)
+}else {
+    console.log(`${num} is a zero number`)
+}
