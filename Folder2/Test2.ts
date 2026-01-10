e4678868d92296fbfaea8a478a89e6a08e56a611
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-10 11:17:44 +0530

    11:17 am

---
+++ Folder2/Test2.ts
@@ -0,0 +1,23 @@
+// Increment 
+//Decrement
+
+let x = 10;
+x =x+1;
+console.log(x, "x")
+
+let x2 =10;
+ // when we have written the Postincrement first 
+ // it will assign the value than increment will happen 
+ // so the console out-put will be 10 only 
+
+// let result : number = x2++; 
+// console.log (result,"result")
+let result2 : number = ++x2; 
+console.log (result2,"result2")
+
+
+
+
+
+
+
---
+++ Folder2/Test3.ts
@@ -0,0 +1,9 @@
+let y =10;
+// y = y-1
+// y--;
+
+
+let result = y--;
+console.log(result)
+console.log(y)
+
