629df50d177f75db4eeca5b739044efee4ab8340
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-10 15:44:58 +0530

    10-01-2026 03:44:45

---
+++ Folder2/Test5.ts
@@ -0,0 +1,6 @@
+// let x = 3 ** 2
+// console.log(x)
+
+// let a : number =30;
+
+console.log(10>=5);
---
+++ Folder2/Test6.ts
@@ -0,0 +1,16 @@
+let a =10;
+let b =20;
+let c =30;
+
+if(a>b && a>c){
+    console.log (a , "Largest Number")
+}else if(b>c && b>a) {
+    console.log (b , "Largest Number Number")
+}else {
+    console.log (c , "Largest Number")
+}
+
+
+
+
+
