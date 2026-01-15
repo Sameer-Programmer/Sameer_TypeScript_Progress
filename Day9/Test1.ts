769030e807b50a84e188cbb4414d5224a66a4831
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-15 18:04:37 +0530

    Jan-15-2026 06:04:PM

---
+++ Day9/Test1.ts
@@ -0,0 +1,14 @@
+function m1(Patient_age :number){
+    console.log(Patient_age)
+}
+
+m1(2);
+
+function m2(name :string , callback :(age:number)=>void)
+{
+    console.log(name)
+    callback(2)
+}
+
+
+m2(" Ameera",m1)
---
+++ Day9/tempCodeRunnerFile.ts
@@ -0,0 +1,13 @@
+function m1(Patient_age :number){
+    console.log(Patient_age)
+}
+
+m1(2);
+
+function m2(name :string , callback :(age:number)){
+    console.log(name)
+    callback(2)
+}
+
+
+m2(" Ameera",m1)
