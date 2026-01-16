945e1183d2ae22d94b1813c968e4eb349571a300
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-16 23:51:11 +0530

    Jan-16-2026 11:50:PM

---
+++ Day11_TupleConcept/Test1.ts
@@ -0,0 +1,11 @@
+/*
+- Tuples in typescript
+- A tuple is a fixed length array where each element  has a specific type
+- It helps in storing Multiple fields of different data types together
+
+*/
+
+let person : [string , number , ] = ["sameer" , 30]
+
+console.log(person[0])
+console.log(person[1])
