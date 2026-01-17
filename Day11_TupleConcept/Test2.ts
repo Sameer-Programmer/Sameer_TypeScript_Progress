20b2134baaabfaf46e0b1ad66c2f478c638f2a30
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-17 08:50:24 +0530

    Jan-17-2026 08:50:AM

---
+++ Day11_TupleConcept/Test2.ts
@@ -0,0 +1,13 @@
+// tuple concept
+
+let basket1 : [number , string ][]  =[ [ 1, "sameer"] , [ 2, "imran"]
+]
+
+for(let i in basket1){
+  
+  console.log(basket1[i])
+  let temp = basket1 [i]
+  console.log(temp[0])
+  break;
+}
+
