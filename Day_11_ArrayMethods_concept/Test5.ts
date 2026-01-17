774fc777db184ee179295e6ade212317f8a30f35
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-17 22:09:57 +0530

    Jan-17-2026 10:09:PM

---
+++ Day_11_ArrayMethods_concept/Test5.ts
@@ -0,0 +1,30 @@
+
+/* splice()
+add/removes elements from an array 
+// syntax - array.splice(start , deletecount )
+
+
+indexof()- finds the index of an element , if element not found return -1 
+// syntax -: array.indexOf(search for an element )
+- array.indexof(searchElement , starting Index )
+*/
+
+
+
+let cart2 : string [] = ["Apple","Banana","Carrot","Dragon Fruit"," Kiwi"]
+
+
+console.log(cart2)
+
+
+
+// let place = cart.indexOf("arrot")
+ 
+// console.log(cart)
+
+// console.log(place)
+ 
+ 
+  let place2= cart2.indexOf("Mango")
+  
+console.log(place2)
---
+++ Day_11_ArrayMethods_concept/Test6.ts
@@ -0,0 +1,21 @@
+
+/* includes() - checks element exist 
+it always return // true or false 
+ 
+// syntax = cart .includes(search element )
+
+optional
+//syntax = cart .includes(search element , fromindex )
+
+
+
+*/
+
+
+
+let cart3 : string [] = ["Apple","Banana","Carrot","Dragon Fruit"," Kiwi"]
+
+
+let isfind = cart3.includes("Apple")
+
+console.log(isfind)
