30ac26c045f72228deb12013f46e2b412eaf27a9
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-18 11:19:15 +0530

    Jan-18-2026 11:19:AM

---
+++ Day_11_Advanced_ArrayMethods_concept/Test2.ts
@@ -0,0 +1,33 @@
+
+let shoppingcart : string [] = [" sameer", " Imran", " Peera"]
+
+// let cart2 : string [] = []
+
+
+shoppingcart.forEach((element ) =>{
+  
+  console.log (element.toUpperCase())
+  
+}
+);
+
+
+
+
+
+// for (let i in cart ){
+//   cart2[i] = cart[i].toUpperCase()
+//   console.log( cart2[i] )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
