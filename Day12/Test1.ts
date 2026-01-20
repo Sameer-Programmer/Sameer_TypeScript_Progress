0f0f209e123589efa42072f08c5ab1a9114a1688
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-20 09:40:38 +0530

    "Jan-20-2026 09:40:AM"

---
+++ Day12/Test1.ts
@@ -0,0 +1,19 @@
+
+/*
+String - Text Value or Combination of characters 
+-Single Quote - String Lteral ('Single quote')
+- Bouble quote - String literal (" Double Quote")
+- backtick (``) - when we try to print value in side a string 
+- Index in string begin with zero 
+
+
+*/
+
+let cart1 :string = "sameer";
+let cart2 :string = 'sameer';
+let cart3 :string = `sameer`;
+// Above 3 are valid 
+
+console.log(cart1 )
+console.log(cart2 )
+console.log(cart3 )
---
+++ Day12/Test2.ts
@@ -0,0 +1,16 @@
+let cart4 :string = "sameer";
+
+// To find the length 
+
+
+let ccart1 :string = "sameer";
+let ccart2 :string = "Mango   ";
+let ccart3 :string = "Mango@#$   ";
+
+console.log(ccart1.length)
+console.log(ccart2.length)
+console.log(ccart3.length)
+
+
+// legth will starts from 1 
+//legth consider Space , special character 
