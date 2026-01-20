ef11502284c7430fe890b4dcfa92692aad679509
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-20 12:53:16 +0530

    Jan-20-2026 12:53:PM

---
+++ Day12/Test3.ts
@@ -0,0 +1,23 @@
+
+let basketcart :string = "sameer , Imran";
+
+// console.log(cbasketcart)
+// console.log(cbasketcart.charAt(0))
+// console.log(cbasketcart.indexOf("sameer"))
+
+// substring()
+// console.log(cbasketcart.substring(0,5))
+
+// substring will take starting index and ending index 
+//  starting index  count from 0 
+// ending index index  count from 1
+
+// includes():  returns True or false 
+// It is Case sensitive 
+
+
+console.log(basketcart.includes("sameer"))// true
+
+// case Sensitive
+
+console.log(basketcart.includes("SAMEER"))// false 
