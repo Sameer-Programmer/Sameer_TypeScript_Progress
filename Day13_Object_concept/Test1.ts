96b2e0faa4190de46ddd35b990380f91b0c1f01f
Sameer-Programmer <sameeridea7@gmail.com>
2026-01-22 15:27:56 +0530

    "Jan-22-2026 03:27:PM"

--- Day13_Object_concept/Test1.ts
+++ Day13_Object_concept/Test1.ts
@@ -72,34 +72,3 @@ employee.age = 31;
 
 console.log(employee["name"], employee["age"])
 
-//=========================================================
-// 2 - InlineType Object - we also define the datatype of the key (TS)
-// inside the Object It self we Sepecify the Type 
-
-
-// Object Declaration – structure of the object
-
-/*
-🔹 Interview-ready answer 🔥
-
-Arrow functions should not be used for object or
- class methods because they do not have their own this. 
- Instead, they capture this from the surrounding scope, 
- which leads to incorrect object references.
-*/
-let Car: {
-  name: string;
-  model: number;
-  safetyGrade: string;
-  getSummary: () => void; // arrow function returning nothing
-} = {
-  name: "AUDI",
-  model: 2025,
-  safetyGrade: "A",
-  getSummary:function  ()  {
-    console.log(
-      `Car Name: ${this.name}, Model: ${this.model}, Safety Grade: ${this.safetyGrade}`
-    );
-  }
-};
-Car.getSummary();
