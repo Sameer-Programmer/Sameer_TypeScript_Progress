f4d11c8907024ec30876c9bf9ad96d358ff21baa
Sameer <sameeridea7@gmail.com>
2026-02-21 15:39:28 +0530

    "Feb-21-2026 03:39:PM"

--- RevisonFolder/Day1_Practice/Test4.ts
+++ RevisonFolder/Day1_Practice/Test4.ts
@@ -33,3 +33,74 @@ if(x6>0){
 }else {
   console.log("Negative")
 }
+
+
+//5. Check if a character is an uppercase vowel..
+
+
+let x7:string  = "E"
+
+if((x7==="A" || x7==="E") && (x7==x7.toUpperCase()) 
+                        && (x7!==x7.toLowerCase())){
+  console.log("given chareter is upper vowel ")
+}
+
+
+let x8:string  = "E"
+
+if((x8=="A" || x8=="E"||  x8=="I" || x8=="O"  || x8=="U")) {
+  console.log("given chareter is upper vowel ")
+}
+
+
+//  Check if a number is a multiple of both 5 and 10.
+
+
+let x9 = 30;
+
+if(x9%5 ==0 && x9%10==0){
+  console.log("multiple of both 5 and 10")
+}else {
+  console.log("NOT multiple of both 5 and 10")
+}
+
+
+// Print the corresponding month name for a given month number.
+
+
+let monthNumber = 4;
+
+switch (monthNumber){
+   case 1 : console.log("jan") ;
+   break;
+   
+    case 2: console.log("Feb") ;
+    break;
+   
+    case 3 : console.log("March") ;
+    break;
+   
+    case 4 : console.log("April ") ;
+    break;
+   
+   default :  console.log("Enter Number from 1 -12") ;
+  
+}
+//Perform basic arithmetic operations based on user input..
+
+let a =10;
+let b =20;
+
+let operator = "";
+
+switch (operator){
+  
+  case "+" : console.log(a+b) ;
+  break;
+  case "-" : console.log(a-b) ;
+  break;
+  
+  default : console.log("please enter the opertor + or - or / or *")
+}
+
+
