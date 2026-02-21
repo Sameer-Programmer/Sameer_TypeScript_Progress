0c09801262d00d8a06f3e1d31caffc424199fb77
Sameer <sameeridea7@gmail.com>
2026-02-21 13:59:37 +0530

    "Feb-21-2026 01:59:PM"

---
+++ RevisonFolder/Day1_Practice/Test3.ts
@@ -0,0 +1,11 @@
+let x1;  // Declartion is allowed 
+//let x1; //Redeclaration is NOT allowed for let in TypeScript
+
+// const y1 ; //-- Declaration is not  allowed for Const variables must be initialized at the time of declaration in TypeScript
+
+// y1 =20;
+
+
+let z1 =300;
+z1=220;
+console.log(z1)
---
+++ RevisonFolder/Day1_Practice/Test4.ts
@@ -0,0 +1,35 @@
+//Check if a character is uppercase
+
+console.log("test")
+
+let x4 = "t"
+
+if(x4 === x4.toUpperCase() && x4!==x4.toLowerCase()){
+  console.log("uppercase")
+}else {
+  console.log("Notuppercase")
+}
+
+
+//Check if a number is a multiple of 10.
+
+let x5 = 1
+
+if(x5%10==0){
+  console.log("Provided number is multiples of 10")
+}else {
+  console.log("Not-multiples of 10")
+}
+
+//5. Check if a number is positive, negative, or zero.
+
+
+let x6 = -1
+
+if(x6>0){
+  console.log(" positive")
+}else if (x6==0){
+   console.log("zero")
+}else {
+  console.log("Negative")
+}
