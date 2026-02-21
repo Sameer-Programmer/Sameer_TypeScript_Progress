86d64bcdebcb81f23e73a179d2eabd9ec24e5668
Sameer <sameeridea7@gmail.com>
2026-02-21 15:56:35 +0530

    [LOG]: "Feb-21-2026 03:56:PM"

---
+++ RevisonFolder/Day1_Practice/Test5.ts
@@ -0,0 +1,22 @@
+// Loops
+
+/*
+When working with loops, we need to provide 3 things:
+
+1. Initialization
+2. Condition
+3. Increment (or Decrement)
+
+*/
+
+// whileLoop --keep on Executing untill the condition is true 
+
+
+let i = 1;
+
+while(i<=6){
+    console.log(i)
+    i++;
+}
+
+
---
+++ RevisonFolder/Day1_Practice/Test6.ts
@@ -0,0 +1,29 @@
+// do whileloop 
+
+
+//syntax 
+
+/*
+do {
+  
+}while(condiiton )
+
+*/
+
+let i = 6 ;
+
+do {
+  console.log(i) //6
+  i++; //7
+}while(i<=7)
+
+
+/*
+//output
+Output:
+
+176 ms
+6
+7
+
+*/
