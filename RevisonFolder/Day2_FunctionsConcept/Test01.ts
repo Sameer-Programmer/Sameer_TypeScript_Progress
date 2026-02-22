b583aee24e046fb5cea93f34fb270aa067bddb22
Sameer <sameeridea7@gmail.com>
2026-02-22 11:40:33 +0530

    "Feb-22-2026 11:40:AM"

---
+++ RevisonFolder/Day2_FunctionsConcept/Test01.ts
@@ -0,0 +1,43 @@
+/*
+
+- Named function 
+- Anonymus function 
+- Arrow function 
+- Restparameter 
+-Optional parameters 
+- default parameters 
+
+*/
+
+
+
+// Named function 
+
+function m1 ():void {
+console.log("Sameer")
+}
+
+m1()
+
+
+function m2 ():number {
+return 5;
+}
+
+console.log(m2())
+
+
+// Anonymus function 
+
+let resulty = function (a:number , b:number):void {
+
+    let sum = a+b
+    console.log(sum)
+
+}
+resulty(1,2)
+
+
+// Lamba function 
+
+
--- RevisonFolder/Day2_FunctionsConcept/Test02.ts
+++ RevisonFolder/Day2_FunctionsConcept/Test02.ts
@@ -1,30 +1,104 @@
-// Named function 
+// Lamda Function or Arrow Function
 
-function m1 ():void {
-console.log("Sameer")
+/*
+
+//// Arrow-Functions/Lamda-Function
+
+   Lambda refers to anonymous functions in programming.
+   Lambda functions are a concise mechanism to represent anonymous functions.
+   These functions are also called as Arrow functions.
+
+   🎯 Interview Answer (Short Version)
+
+Arrow functions provide shorter syntax and use lexical this, meaning they inherit this from their surrounding scope.
+
+   There are 3 parts to a Lambda function.
+
+   1. Parameters – A function may optionally have parameters
+   2. The fat arrow notation/lambda notation (=>) – It is also called as the goes to operator
+   3. Statements – represent the functions instruction set
+
+   Syntax:
+
+   let variable = (parameters) =>
+   {
+       // block of code
+   }
+
+   variable();
+*/
+
+
+// Void Type
+let c3 = ()=> {
+    console.log("Arrow function")
 }
 
-m1()
+
+c3();
 
 
-function m2 ():number {
-return 5;
+// Return 
+
+let c4 = ():string => {
+    return "sameer"
+}
+console.log(c4())
+
+
+// RestParameters 
+
+let c5 = (...elements:number[])=>{
+    console.log(elements.length)
 }
 
-console.log(m2())
+c5(4,5)
 
 
-// Anonymus function 
-
-let resulty = function (a:number , b:number):void {
-
-    let sum = a+b
-    console.log(sum)
-
+//
+let c6 = (...elements:(number|string)[])=>{
+    console.log(elements.length)
 }
-resulty(1,2)
+
+c6(4,5,"sameer","Imran")
 
 
-// Lamba function 
+/*
+
+function -- Named , Anonymus , arrow ( void , return type) 
+- Rest Parameters - (...elements:number[])
+-default parameters -- (c:number =0 )  
+// Here directly we will assign the default value 
+// if we provide while giving input i will take if you dont provide it will assign this default vale
+
+*/
+
+// optional Parameters 
+
+/*
+- optional parameters always implement atlast
+- this Optional parameter we have to handle with if condition due to undefined if we dont assign or provide the Optional value 
+*/
+let x13 = (a:number , b:number , c?:number)=>{
+ 
+ if(c!==undefined){
+     return a+b+c
+ }else{
+     return a+b 
+ }
+}
+
+console.log(x13(11,2))
 
 
+// default parameters always be at last 
+
+// Confusion part is we should not provide question mark before ?: for c -- 
+// ? mark indicates Optional 
+// here it is Default Parameter -- we are assigning directly zero 
+
+let y3 = (a: number, b: number, c: number = 0) => {
+  return a + b + c;
+}
+
+console.log(y3(1,2))
