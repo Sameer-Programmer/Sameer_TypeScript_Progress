c5cf5d2eb2859dbec53b170807ab399abf695f6a
Sameer <sameeridea7@gmail.com>
2026-02-23 12:43:03 +0530

    "Feb-23-2026 12:42:PM"

---
+++ RevisonFolder/Day4_ArrayMethods_Concept/Test1.ts
@@ -0,0 +1,59 @@
+/* 
+we have many methods in Array 
+push()- to add - at last 
+pop () - to remove the last element 
+shift () - to remove First Element 
+unshift() - to add at First element 
+concat () - to add two arrays 
+indexOf(1) Returns the index of the first occurrence of a value, or -1 if not found.
+includes() - if found true - if not false 
+slice () - From the Original array if we need some part with index 
+slice ()
+
+// Strating index starts from zero
+// Ending index will be eclusive - n-1 
+// syntax = slice (start , end )
+
+
+*/
+
+let elements1 : (number|string|boolean)[] = [1, "sameer", true ]
+
+
+console.log(elements1)
+console.log(typeof elements1)
+
+elements1.push()
+// Add Element at last  inside method we should pass some thing if we wont pass what will happen- Array will remain same  
+console.log(elements1 + "        push")  
+
+elements1.push(3)
+console.log(elements1 + "        push")  
+
+
+elements1.pop()
+console.log(elements1) // Remove element at Last 
+
+elements1.shift()
+console.log(elements1) // Remove element at First
+
+
+elements1.unshift("Apple")
+console.log(elements1) // ADD  element at First
+
+// To add two Arrays
+
+let e2 = [1,2]
+let e3 = ["sameer","imran"]
+
+let result3 = e2.concat(e3) 
+//The red mark under e3 is because of a TypeScript type mismatch.- later while running Js - des not care - typescript makes inference 
+console.log(result3) 
+
+
+//let elements1 : (number|string|boolean)[] = [1, "sameer", true ]
+
+console.log(elements1+"        present") 
+console.log(typeof elements1)
+let result_slice = elements1.slice(0,3) //(start =0, end = 3-1 = 2)
+console.log(elements1) 
---
+++ RevisonFolder/Day4_ArrayMethods_Concept/Test2.ts
@@ -0,0 +1,39 @@
+// 7. splice()
+
+
+//Description: Adds or removes elements at any position in the array.
+
+//syntax  arrayname .splice (entryindex to delete, how many items to delete , Add ) // add is optional if you want to add any string or number
+
+/*
+| deleteCount | Meaning                                    |
+| ----------- | ------------------------------------------ |
+| 0           | Delete nothing                             |
+| 1           | Delete 1 element                           |
+| 2           | Delete 2 elements                          |
+| Not given   | Delete all elements from startIndex to end |
+
+ */
+
+let elements2 : (number|string|boolean)[] = [1, "sameer", true, "Apple","Banana" ]
+
+elements2.splice(1,2)  
+// sameer , sameer and true will be deleted 
+// from index 1 delete starts and anothe 2 indicates two items u are going to delete from tht index 
+console.log(elements2)
+
+//now to add 
+
+elements2.splice(1,0,"Pine" ) // After 1 I am going to add , 0 means no deted 
+console.log(elements2)
+// is here zero indicated no deleted
+// output
+// [ 1, 'Pine', 'Apple', 'Banana' ]
+
+
+// indexOf(1) Returns the index of the first occurrence of a value, or -1 if not found.
+
+console.log(elements2.indexOf(1))
+
+console.log(elements2.includes(1))  // if found true - if not found -- false 
+console.log(elements2.includes(11))
