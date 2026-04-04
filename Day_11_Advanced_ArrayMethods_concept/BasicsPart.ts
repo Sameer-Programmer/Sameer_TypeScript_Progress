309f1a3d09c10a5e0e875002dd0c3acc187290fe
Sameer <sameeridea7@gmail.com>
2026-04-04 09:37:27 +0530

    feat: add reference examples for advanced array methods in TypeScript

---
+++ Day_11_Advanced_ArrayMethods_concept/BasicsPart.ts
@@ -0,0 +1,39 @@
+
+// ===== forEach =====
+array.forEach((element, index, array) => {
+  // logic
+});
+
+
+// ===== map =====
+array.map((element, index, array) => {
+  return newValue;
+});
+
+
+// ===== filter =====
+array.filter((element, index, array) => {
+  return condition;
+});
+
+
+// ===== reduce =====
+array.reduce((accumulator, currentValue, index, array) => {
+  return updatedAccumulator;
+}, initialValue);
+
+
+// ===== every =====
+array.every((element, index, array) => {
+  return condition;
+});
+
+
+// ===== some =====
+array.some((element, index, array) => {
+  return condition;
+});
+
+
+// ===== indexOf =====
+array.indexOf(searchElement, fromIndex);
